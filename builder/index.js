57fab18ce207b260ce706a3cb2bf376da7b8fb5d
niklasschroetler <privat@nschroetler.de>
2019-10-02 21:43:21 +0200

    Added four new badge types and a builder

---
+++ builder/index.js
@@ -0,0 +1,145 @@
+// * VARIABLES * //
+var genurl = "https://jhbadge.de/"; 			// URL of the generator
+
+var types = [                       			// Types of Badges (names) 
+	"by-alpacas",
+	"created-for",
+	"standard",
+	"started-at",
+	"view-presentation"
+];
+var events = [                      			// List of Events and Abbreviations
+	{
+		name: "Berlin",
+		abbr: "ber"
+	},
+	{
+		name: "Frankfurt",
+		abbr: "ffm"
+	},
+	{
+		name: "Halle",
+		abbr: "hal"
+	},
+	{
+		name: "Hamburg",
+		abbr: "hh"
+	},
+	{
+		name: "Heidelberg",
+		abbr: "hd"
+	},
+	{
+		name: "Köln",
+		abbr: "cgn"
+	},
+	{
+		name: "MV | Rostock",
+		abbr: "mv"
+	},
+	{
+		name: "Ulm",
+		abbr: "ulm"
+	},
+	{
+		name: "Österreich",
+		abbr: "at"
+	},
+	{
+		name: "Schweiz",
+		abbr: "ch"
+	},
+	{
+		name: "Asia",
+		abbr: "asia"
+	},
+];
+
+var typeSelected = "";              			// Type of Badge selected (part of types array)
+var eventSelected = "ber";                      // Event selected (abbreviation)
+var yearSelected = new Date().getFullYear();    // Year selected (initialises with current year)
+
+
+// * INIT * //
+// Add the JugendHackt events to the event selector 
+events.forEach(evt => {
+	document.getElementById("events").innerHTML += `
+		<div id="event-${evt.abbr}" onclick="selectEvent('${evt.abbr}')">
+			<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="90%" viewBox="0 0 11 11" id="jh_logo" shape-rendering="crispEdges" event="${evt.abbr}">
+				<g id="face">
+					<g id="eyes">
+					<rect id="left_eye" x="2" y="6" width="1" height="1" ></rect>
+					<rect id="right_eye" x="8" y="6" width="1" height="1" ></rect>
+					</g>
+					<polygon id="mouth"
+					points="4,7 4,9 7,9 7,7 6,7 6,8 5,8 5,7" ></polygon>
+				</g>
+				<g id="head">
+					<polygon id="ears"
+					points="1,0 3,0 3,1 4,1 4,2 7,2 7,1 8,1 8,0 10,0 10,1 11,1 11,5 9,5 9,4 2,4 2,5 0,5 0,1 1,1" ></polygon>
+					<polygon id="beard"
+					points="0,5 0,9 1,9 1,10 2,10 2,11 9,11 9,10 10,10 10,9 11,9 11,5 10,5 10,8 9,8 9,9 8,9 8,10 3,10 3,9 2,9 2,8 1,8 1,5" ></polygon>
+				</g>
+			</svg>
+			<p>${evt.name}</p>
+		</div>`;
+});
+
+// Fill HTML Elements with standard values / select standard selections
+document.getElementById("badges-standard").setAttribute("selected", "true");
+document.getElementById("event-ber").setAttribute("selected", "true");
+document.getElementById("year").value = yearSelected;
+
+// Refresh the Preview with the new variables
+refreshIFrame();
+
+
+// * FUNCTIONS * //
+// Onclick event for all type select divs
+function selectType(abbr) {
+	if(abbr != "standard") { 	// If standard, leave abbr empty
+		typeSelected = abbr;	// Else, save the selection
+	}
+	else {
+		typeSelected = "";
+	}
+	types.forEach(type => {		// remove all selected borders
+		document.getElementById("badges-"+type).setAttribute("selected", "false");
+	});
+	document.getElementById("badges-"+abbr).setAttribute("selected", "true");	// add a selected border to the clicked on element
+
+	refreshIFrame();	// Refresh the preview with the new variables
+}
+
+// Onclick event for all event select divs
+function selectEvent(abbr) {
+	eventSelected = abbr;	// Save the event selection
+	events.forEach(evt => {	// remove all selected borders
+		document.getElementById("event-"+evt.abbr).setAttribute("selected", "false");
+	});
+	document.getElementById("event-"+abbr).setAttribute("selected", "true");	// add a selected border to the clicked on element
+
+	refreshIFrame();	// Refresh the preview with the new variables
+}
+
+// Onchange and oninput event for the year select input
+function selectYear() {
+	yearSelected = document.getElementById("year").value;	// Save the new year
+	refreshIFrame();	// Refresh the preview with the new variables
+}
+
+// Refreshes the content of the preview iFrame with the current variables. Also refreshes the 
+function refreshIFrame() {
+	var url = "";
+	if(typeSelected == "") {	// If standard, don't add a type GET Variable for simplicity
+		url = genurl + "?evt=" + eventSelected + "&year=" + yearSelected;
+	}
+	else {
+		url = genurl + "?type=" + typeSelected + "&evt=" + eventSelected + "&year=" + yearSelected;
+	}
+
+	// Fill the HTML attributes with the new URL (in various forms)
+	document.getElementById("preview").src = url;
+	document.getElementById("url").innerHTML = '<a href="' + url + '">'+url+'</a>';
+	document.getElementById("md").innerText = "![](" + url + ")";
+}
--- index.php
+++ index.php
@@ -89,19 +89,94 @@
             break;
     }
 
-    echo('<svg xmlns="http://www.w3.org/2000/svg" width="'.$viewWidth.'" height="20" viewBox="0 0 '.$viewWidth.' 20">');
-    echo('<a href="https://jugendhackt.org">');
-    echo('<title>JH'.$evtAbbr.' Badge</title>');
-    echo('<rect x="0" y="0" width="136" height="20" style="fill:#555"/>');
-    echo('<rect x="135" y="0" width="7000" height="20" style="fill:'.$evtColor.'"/>');
-    
-    echo('<text transform="translate(8 14)" style="isolation:isolate;font-size:11px;fill:#fff;" font-family="Verdana,Geneva,sans-serif">Made at JugendHackt</text>');
-    echo('<text transform="translate(160 14)" style="isolation:isolate;font-size:11px;fill:#fff;" font-family="Verdana,Geneva,sans-serif">'.$evtLong.' '.$evtYear.'</text>');
 
-    echo('<rect x="144.9" y="10.55" width="1.09" height="1.09" style="fill:#fff"/>');
-    echo('<rect x="151.46" y="10.55" width="1.09" height="1.09" style="fill:#fff"/>');
-    echo('<polygon points="149.28 12.73 148.18 12.73 148.18 11.64 147.09 11.64 147.09 13.83 150.37 13.83 150.37 11.64 149.28 11.64 149.28 12.73" style="fill:#fff"/>');
-    echo('<path d="M153.65,5.08V4h-2.19V5.08h-1.09V6.17h-3.28V5.08H146V4h-2.19V5.08h-1.09v8.75h1.09v1.09h1.09V16h7.66V14.92h1.09V13.83h1.09V5.08Zm0,7.65h-1.09v1.1h-1.1v1.09H146V13.83h-1.1v-1.1h-1.09V9.45h1.09V8.36h7.66V9.45h1.09Z" style="fill:#fff"/>');
-    echo('</a>');
-    echo('</svg>');
+    switch ($_GET['type']) {
+        case 'created-for':
+            echo('<svg xmlns="http://www.w3.org/2000/svg" width="226.5" height="20" viewBox="0 0 226.5 20">');
+            echo('<title>JH'.$evtAbbr.' Badge</title>');
+            echo('<a href="https://jugendhackt.org">');
+            echo('<rect x="0" y="0" width="170" height="20" style="fill:#555"/>');
+            echo('<rect x="169" y="0" width="58" height="20" style="fill:'.$evtColor.'"/>');
+            echo('<text transform="translate(8.53 14.52)" style="isolation: isolate;font-size: 11px;fill: #fff;font-family: Verdana">Created for JugendHackt in</text>');
+            echo('<g>');
+            echo('<text transform="translate(192.62 14.27)" style="isolation: isolate;font-size: 11px;fill: #fff;font-family: Verdana">'.$evtYear.'</text>');
+            echo('<g>');
+            echo('<rect x="176.76" y="10.53" width="1.09" height="1.09" style="fill: #fff"/>');
+            echo('<rect x="183.32" y="10.53" width="1.09" height="1.09" style="fill: #fff"/>');
+            echo('<polygon points="181.13 12.72 180.04 12.72 180.04 11.62 178.95 11.62 178.95 13.81 182.23 13.81 182.23 11.62 181.13 11.62 181.13 12.72" style="fill: #fff"/>');
+            echo('<path d="M185.51,5.06V4h-2.19V5.06h-1.09v1.1H179V5.06h-1.09V4h-2.19V5.06h-1.1v8.75h1.1V14.9h1.09V16h7.66V14.9h1.09V13.81h1.09V5.06Zm0,7.66h-1.09v1.09h-1.1V14.9h-5.46V13.81h-1.1V12.72h-1.09V9.44h1.09V8.34h7.66v1.1h1.09Z" style="fill: #fff"/>');
+            echo('</g>');
+            echo('</g>');
+            echo('</a>');
+            echo('</svg>');
+            break;
+        case 'started-at':
+            echo('<svg xmlns="http://www.w3.org/2000/svg" width="173.85" height="20" viewBox="0 0 173.85 20">');
+            echo('<title>JH'.$evtAbbr.' Badge</title>');
+            echo('<a href="https://jugendhackt.org">');
+            echo('<path d="M23.28,0H173.85V20H26.28Z" style="fill: #555"/>');
+            echo('<path d="M0,0H26.28V20H0Z" style="fill: '.$evtColor.'"/>');
+            echo('<text transform="translate(34.81 14.51)" style="isolation: isolate;font-size: 11px;fill: #fff;font-family: Verdana">Started at JugendHackt</text>');
+            echo('<g>');
+            echo('<rect x="9.31" y="10.53" width="1.09" height="1.09" style="fill: #fff"/>');
+            echo('<rect x="15.87" y="10.53" width="1.09" height="1.09" style="fill: #fff"/>');
+            echo('<polygon points="13.69 12.72 12.59 12.72 12.59 11.62 11.5 11.62 11.5 13.81 14.78 13.81 14.78 11.62 13.69 11.62 13.69 12.72" style="fill: #fff"/>');
+            echo('<path d="M18.06,5.06V4H15.87V5.06H14.78v1.1H11.5V5.06H10.41V4H8.22V5.06H7.12v8.75h1.1V14.9H9.31V16H17V14.9h1.09V13.81h1.09V5.06Zm0,7.66H17v1.09h-1.1V14.9H10.41V13.81H9.31V12.72H8.22V9.44H9.31V8.34H17v1.1h1.09Z" style="fill: #fff"/>');
+            echo('</g>');
+            echo('</a>');
+            echo('</svg>');
+            break;
+        case 'by-alpacas':
+            echo('<svg xmlns="http://www.w3.org/2000/svg" width="149.79" height="20" viewBox="0 0 149.79 20">');
+            echo('<title>JH'.$evtAbbr.' Badge</title>');
+            echo('<a href="https://jugendhackt.org">');
+            echo('<path d="M25.28,0H149.79V20H26.28Z" style="fill: #555"/>');
+            echo('<path d="M0,0H26.28V20H0Z" style="fill: '.$evtColor.'"/>');
+            echo('<text transform="translate(34.81 14.51)" style="isolation: isolate;font-size: 11px;fill: #fff;font-family: Verdana">Created <tspan x="47.14" y="0" style="letter-spacing: -0.00244140625em">b</tspan><tspan x="53.97" y="0">y Alpacas</tspan></text>');
+            echo('<g>');
+            echo('<rect x="9.31" y="10.53" width="1.09" height="1.09" style="fill: #fff"/>');
+            echo('<rect x="15.87" y="10.53" width="1.09" height="1.09" style="fill: #fff"/>');
+            echo('<polygon points="13.69 12.72 12.59 12.72 12.59 11.62 11.5 11.62 11.5 13.81 14.78 13.81 14.78 11.62 13.69 11.62 13.69 12.72" style="fill: #fff"/>');
+            echo('<path d="M18.06,5.06V4H15.87V5.06H14.78v1.1H11.5V5.06H10.41V4H8.22V5.06H7.12v8.75h1.1V14.9H9.31V16H17V14.9h1.09V13.81h1.09V5.06Zm0,7.66H17v1.09h-1.1V14.9H10.41V13.81H9.31V12.72H8.22V9.44H9.31V8.34H17v1.1h1.09Z" style="fill: #fff"/>');
+            echo('</g>');
+            echo('</a>');
+            echo('</svg>');
+            break;
+        case 'view-presentation':
+            echo('<svg xmlns="http://www.w3.org/2000/svg" width="164.83" height="20" viewBox="0 0 164.83 20">');
+            echo('<title>JH'.$evtAbbr.' Badge</title>');
+            echo('<a href="https://media.ccc.de/b/events/jugendhackt">');
+            echo('<path d="M0,0H66.86V20H0Z" style="fill: #555"/>');
+            echo('<path d="M64.86,0h100V20h-100Z" style="fill: '.$evtColor.'"/>');
+            echo('<text transform="translate(8.53 14.52)" style="isolation: isolate;font-size: 11px;fill: #fff;font-family: Verdana">View the</text>');
+            echo('<text transform="translate(89.81 14.29)" style="isolation: isolate;font-size: 11px;fill: #fff;font-family: Verdana">Presentation</text>');
+            echo('<g>');
+            echo('<rect x="73.96" y="10.55" width="1.09" height="1.09" style="fill: #fff"/>');
+            echo('<rect x="80.52" y="10.55" width="1.09" height="1.09" style="fill: #fff"/>');
+            echo('<polygon points="78.33 12.73 77.24 12.73 77.24 11.64 76.14 11.64 76.14 13.83 79.42 13.83 79.42 11.64 78.33 11.64 78.33 12.73" style="fill: #fff"/>');
+            echo('<path d="M82.7,5.08V4H80.52V5.08h-1.1V6.17H76.14V5.08H75.05V4H72.86V5.08H71.77v8.75h1.09v1.09H74V16h7.65V14.92H82.7V13.83h1.1V5.08Zm0,7.65H81.61v1.1H80.52v1.09H75.05V13.83H74v-1.1h-1.1V9.45H74V8.36h7.65V9.45H82.7Z" style="fill: #fff"/>');
+            echo('</g>');
+            echo('</a>');
+            echo('</svg>');
+            break;
+        
+        default:
+            echo('<svg xmlns="http://www.w3.org/2000/svg" width="'.$viewWidth.'" height="20" viewBox="0 0 '.$viewWidth.' 20">');
+            echo('<title>JH'.$evtAbbr.' Badge</title>');
+            echo('<a href="https://jugendhackt.org">');
+            echo('<rect x="0" y="0" width="136" height="20" style="fill:#555"/>');
+            echo('<rect x="135" y="0" width="7000" height="20" style="fill:'.$evtColor.'"/>');
+            
+            echo('<text transform="translate(8 14)" style="isolation:isolate;font-size:11px;fill:#fff;" font-family="Verdana,Geneva,sans-serif">Made at JugendHackt</text>');
+            echo('<text transform="translate(160 14)" style="isolation:isolate;font-size:11px;fill:#fff;" font-family="Verdana,Geneva,sans-serif">'.$evtLong.' '.$evtYear.'</text>');
+        
+            echo('<rect x="144.9" y="10.55" width="1.09" height="1.09" style="fill:#fff"/>');
+            echo('<rect x="151.46" y="10.55" width="1.09" height="1.09" style="fill:#fff"/>');
+            echo('<polygon points="149.28 12.73 148.18 12.73 148.18 11.64 147.09 11.64 147.09 13.83 150.37 13.83 150.37 11.64 149.28 11.64 149.28 12.73" style="fill:#fff"/>');
+            echo('<path d="M153.65,5.08V4h-2.19V5.08h-1.09V6.17h-3.28V5.08H146V4h-2.19V5.08h-1.09v8.75h1.09v1.09h1.09V16h7.66V14.92h1.09V13.83h1.09V5.08Zm0,7.65h-1.09v1.1h-1.1v1.09H146V13.83h-1.1v-1.1h-1.09V9.45h1.09V8.36h7.66V9.45h1.09Z" style="fill:#fff"/>');
+            echo('</a>');
+            echo('</svg>');
+            break;
+    }
+    
 ?>
